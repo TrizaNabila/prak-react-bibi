f75273e401e08a282bf1a075991e601a0d38c9cc
TrizaNabila <triza24si@mahasiswa.pcr.ac.id>
2026-04-28 15:50:43 +0700

    Pertemuan 7

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,51 @@
-import { useState } from "react";
+import { Suspense, useState } from "react";
+import React from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./assets/tailwind.css";
-import Sidebar from "./layouts/Sidebar";
-import Header from "./layouts/Header";
-import Dashboard from "./pages/Dashboard";
 import { Route, Routes } from "react-router-dom";
-import Orders from "./pages/Orders";
-import Customers from "./Pages/Customer";
-import NotFound from "./pages/NotFound";
-
+import Loading from "./components/Loading";
+//import Dashboard from "./pages/Dashboard";
+//import Orders from "./pages/Orders";
+//import Customers from "./pages/Customers";
+//import NotFound from "./pages/NotFound";
+//import MainLayout from "./layouts/MainLayout";
+//import AuthLayout from "./layouts/AuthLayout";
+//import Login from "./pages/auth/Login";
+//import Register from "./pages/auth/Register";
+//import Forgot from "./pages/auth/Forgot";
+const Dashboard = React.lazy(() => import("./pages/Dashboard"))
+const Orders = React.lazy(() => import("./pages/Orders"))
+const Customers = React.lazy(() => import("./pages/Orders"))
+const NotFound = React.lazy(() => import("./pages/NotFound"))
+const MainLayout = React.lazy(() => import("./layouts/MainLayout"))
+const AuthLayout = React.lazy(() => import("./layouts/AuthLayout"))
+const Login = React.lazy(() => import("./pages/auth/Login"))
+const Register = React.lazy(() => import("./pages/auth/Register"))
+const Forgot = React.lazy(() => import("./pages/auth/Forgot"))
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    // Struktur wrapper utama dashboard
-    <div id="app-container" className="bg-gray-100 min-h-screen flex">
-      <div id="layout-wrapper" className="flex flex-row flex-1">
-        {/* Sidebar di sebelah kiri */}
-        <Sidebar />
+    <Suspense fallback={<Loading />}>
+    <Routes>
+      {/* 1. Grup Main Layout */}
+      <Route element={<MainLayout />}>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/customers" element={<Customers />} />
+      </Route>
 
-        {/* Konten utama di sebelah kanan */}
-        <div id="main-content" className="flex-1 p-4 overflow-y-auto">
-          <Header />
-          <Routes>
-            <Route path="*" element={<NotFound />} />
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/orders" element={<Orders />} />
-            <Route path="/customers" element={<Customers />} />
-          </Routes>
-        </div>
-      </div>
-    </div>
+      {/* 2. Grup Auth Layout */}
+      <Route element={<AuthLayout />}>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/forgot" element={<Forgot />} />
+      </Route>
+
+      {/* 3. Catch-all Route (Paling Bawah) */}
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+    </Suspense>
   );
 }
 
